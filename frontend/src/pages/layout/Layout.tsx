ae52e274f9b8f9380172a03e53a35c0a397eff5e
josh-wjp <joshua@wjp.ai>
2024-11-16 10:22:53 -0500

    .svg update for wjp use case

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -4,7 +4,7 @@ import { Dialog, Stack, TextField } from '@fluentui/react'
 import { CopyRegular } from '@fluentui/react-icons'
 
 import { CosmosDBStatus } from '../../api'
-import WJPLogo from '../../assets/WJPLogo.svg'
+import WJPLogo from '../../assets/WJPLogo.png'
 import { HistoryButton, ShareButton } from '../../components/common/Button'
 import { AppStateContext } from '../../state/AppProvider'
 
